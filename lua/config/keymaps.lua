b9921bbc42c154f385c47d6b07f745523dcd3969
Jonh Sousa <jonhnbsousa@gmail.com>
2026-05-05 21:05:27 -0300

    feat - adding keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+-- Atalho para sair do Diffview ou do modo diff nativo
+vim.keymap.set("n", "<leader>gq", function()
+  if vim.wo.diff then
+    vim.cmd("diffoff!")
+  end
+  vim.cmd("DiffviewClose")
+end, { desc = "Sair do modo Diff (Amity Exit)" })
